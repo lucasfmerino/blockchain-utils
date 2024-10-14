f0a48a367d734ead83a755b119d39d2cc093e094
lucasfmerino <lucasfmerino@gmail.com>
2024-10-13 23:27:35 -0300

    feat: added main test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,22 @@ public class App
 {
     public static void main(String[] args)
     {
+        BlockChain blockChain = new BlockChain();
+        Miner miner = new Miner();
 
+        Block genesisBlock = new Block(0, "", Constants.GENESIS_PREV_HASH);
+        miner.mine(genesisBlock, blockChain);
+
+        Block block1 = new Block(1, "transaction1",
+                blockChain.getBlockChain().get(blockChain.getSize()-1).getHash());
+        miner.mine(block1, blockChain);
+
+        Block block2 = new Block(2, "transaction2",
+                blockChain.getBlockChain().get(blockChain.getSize()-1).getHash());
+        miner.mine(block2, blockChain);
+
+
+        System.out.println("\n" + "BLOCKCHAIN:\n" + blockChain);
+        System.out.println("Miner's reward: " + miner.getReward());
     }
 }
--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -5,7 +5,7 @@ public class Constants
 
     }
 
-    public static final int DIFFICULTY = 1;
+    public static final int DIFFICULTY = 5;
     public static final int REWARD = 10;
     public static final String GENESIS_PREV_HASH = "0000000000000000000000000000000000000000000000000000000000000000";
 }
