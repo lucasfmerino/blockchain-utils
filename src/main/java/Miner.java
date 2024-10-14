1223c05c6410847d59d640edb88f78c82e2dac31
lucasfmerino <lucasfmerino@gmail.com>
2024-10-13 23:12:12 -0300

    feat: added Miner

--- src/main/java/Miner.java
+++ src/main/java/Miner.java
@@ -1,4 +1,34 @@
 public class Miner
 {
+    private int reward;
+
+
+    public void mine(Block block, BlockChain blockChain)
+    {
+        while(!isGoldenHash(block))
+        {
+            block.incrementNonce();
+            block.generateHash();
+        }
+
+        System.out.println(block + " has just mined...");
+        System.out.println("Hash is: " + block.getHash());
+
+        blockChain.addBlock(block);
+        reward += Constants.REWARD;
+    }
+
+
+    private boolean isGoldenHash(Block block)
+    {
+        String leadingZeros = new String(new char[Constants.DIFFICULTY]).replace('\0', '0');
+        return block.getHash().substring(0, Constants.DIFFICULTY).equals(leadingZeros);
+    }
+
+
+    public int getReward()
+    {
+        return this.reward;
+    }
 
 }
